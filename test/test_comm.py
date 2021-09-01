aecb74e4d2128f5fe1784ac26847d2f7e0ea4d44
Mike Lee Williams <mike@mike.place>
2021-08-31 22:17:26 -0700

    Add unit tests for comm function
    Closes #5. Will open a new issue for file I/O and cli tests

---
+++ test/test_comm.py
@@ -0,0 +1,39 @@
+import pytest
+from commpy.comm import comm
+
+
+def test_abcd():
+    assert tuple(comm(list("abc"), list("bcd"))) == (
+        ("a", 1),
+        ("b", 3),
+        ("c", 3),
+        ("d", 2),
+    )
+
+
+def test_case():
+    assert tuple(comm(list("abc"), list("Bcd"))) == (
+        ("B", 2),
+        ("a", 1),
+        ("b", 1),
+        ("c", 3),
+        ("d", 2),
+    )
+
+
+def test_ignorecase():
+    assert tuple(comm(list("abc"), list("Bcd"), comptrans=lambda s: s.lower())) == (
+        ("a", 1),
+        ("b", 3),
+        ("c", 3),
+        ("d", 2),
+    )
+
+
+@pytest.mark.xfail
+def test_empty():
+    assert tuple(comm(list("abc"), [])) == (
+        ("a", 1),
+        ("b", 1),
+        ("c", 2),
+    )
